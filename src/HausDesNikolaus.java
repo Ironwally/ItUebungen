bc6876dcb6f4061bf5a5368f15434f29722f4413
Colin Bankart <62899274+Ironwally@users.noreply.github.com>
2022-12-01 10:11:24 +0000

    Haus des Nikolus gratis dazu

--- src/HausDesNikolaus.java
+++ src/HausDesNikolaus.java
@@ -1,5 +1,28 @@
-class HausDesNikolaus {
+class HausDesNikolaus implements TurtlePath {
+
+    public void draw(Turtle t) {
+        t.setPosition(200, 200);
+        t.setAngle(180);
+
+        t.right(90);
+        t.forward(100);
+        t.left(90);
+        t.forward(100);
+        t.left(90);
+        t.forward(100);
+        t.right(90+45);
+        t.forward(Math.sqrt(2)*50);
+        t.right(90);
+        t.forward(Math.sqrt(2)*50);
+        t.right(90);
+        t.forward(Math.sqrt(2)*100);
+        t.right(90+45);
+        t.forward(100);
+        t.right(90+45);
+        t.forward(Math.sqrt(2)*100);
+    }
+
     public static void main(String[] args) {
-        
+        new TurtleCanvas(new HausDesNikolaus());
     }
 }
